ec932f7de7c65f4eade40e08584d1289a311522a
Rémi DEMIR <demir.remi@gmail.com>
2021-09-10 13:23:27 +0200

    feat : add fizzBuzz.js

---
+++ Algo/fizzBuzz.js
@@ -0,0 +1,18 @@
+const fizzBuzz = (length) => {
+  const numbers = [];
+
+  for (let i = 1; i <= length; i += 1) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      numbers.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      numbers.push('Fizz');
+    } else if (i % 5 === 0) {
+      numbers.push('Buzz');
+    } else {
+      numbers.push(i);
+    }
+  }
+  return numbers.join(', ');
+};
+
+console.log(fizzBuzz(45));
