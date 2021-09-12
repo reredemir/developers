aa13ae46857375037b71e805e14dfbdfaf2c34a7
Rémi DEMIR <demir.remi@gmail.com>
2021-09-12 13:21:15 +0200

    docs : add commented code for customNumberTypeIncrement

--- Algo/customNumberTypeIncrement.js
+++ Algo/customNumberTypeIncrement.js
@@ -2,30 +2,30 @@ const customNumberTypeIncrement = (num) => {
   //On retourne le tableau pour pouvoir le parcourir dans le bon sens dans un for of
   num.reverse();
 
-  //on incrémente le premier digit
+  //On incrémente le premier digit
   num[0]++;
 
   num.forEach((digit, index) => {
     //Si le digit est à 10
     if (digit === 10) {
-      //Alors je passe le digit à 0
+      //alors je passe le digit à 0
       num[index] = 0;
-      //Ensuite, si le digit est le dernier digit du tableau, j'ajoute un digit '1'
+      //ensuite, si le digit est le premier digit du tableau, j'ajoute un digit '1'
       if (index === num.length - 1) {
         num.push(1);
       } else {
-        //Sinon, si le digit n'est pas le dernier digit du tableau, j'incrémente le digit d'après
-        num[index + 1]++
+        //sinon, si le digit n'est pas le prermier digit du tableau, j'incrémente le digit d'après
+        num[index + 1]++;
       }
     }
-  })
+  });
 
   //Je remets le tableau à l'endroit
   num.reverse();
 
   return num;
-}
+};
 
-console.log(customNumberTypeIncrement([9, 9]))
+console.log(customNumberTypeIncrement([9, 9]));
 
 module.exports = customNumberTypeIncrement;
